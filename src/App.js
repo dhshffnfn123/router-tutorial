2c890b63989b35c2d034e9d29deaee785c396ae9
B H An <anbyunghoon0107@gamil.com>
2021-11-10 23:39:50 +0900

    URL 파라미터와 쿼리

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import About from "./About";
 import Home from "./Home";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
+import Profile from "./Profile";
 
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/profile/velopert">Velopert Profile</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">홍길동의 Profile</Link>
+        </li>
+      </ul>
+      <hr />
         <Route path="/" component={Home}  exact={true}/>
-        <Route path="/about" component={About} />
+        <Route path={['/about', '/info']} component={About} />  {/* 배열을 사용한 Link */}
+        <Route path="/profile/:username" component={Profile} />
     </div>
   );
 };
---
+++ src/Profile.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const data = {
+    velopert : {
+        name : '안병훈',
+        description : '리액트를 시작하는 개발자'
+    },
+    gildong : {
+        name : '홍길동',
+        description : '고전 소설 홍길동의 주인공'
+    }
+};
+
+const Profile = ({ match }) => {
+    const { username } = match.params;  // username 값 조회
+    const profile = data[username];
+    if (!profile) {
+        return <div>존재하지 않는 사용자 입니다.</div>
+    }
+    return (
+        <div>
+            <h3>
+                {username}({profile.name})
+            </h3>
+            <p>{profile.description}</p>
+        </div>
+    )
+}
+
+export default Profile;
